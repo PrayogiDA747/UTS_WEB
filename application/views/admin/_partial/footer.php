7bd26466832027e0343adb7db2bf32b1e88c1a6b
PrayogiDA747 <muhammadagungprayogi13@gmail.com>
2020-04-30 23:42:29 +0700

    first commit

---
+++ application/views/admin/_partial/footer.php
@@ -0,0 +1,7 @@
+  <footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>Version</b> 2.4.18
+    </div>
+    <strong>Copyright &copy; 2020 Muhammad Agung Prayogi (181240000747) </strong> All rights
+    reserved.
+  </footer>
